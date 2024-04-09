27b2b2c75771f49ac79c9e0f1e5cbe31021c3817
정채은 <cce523@hanmail.net>
2024-04-09 14:57:18 +0900

    feat: 출금 로직 구현

--- firstSeminar/src/main/java/org/example/BankMain.java
+++ firstSeminar/src/main/java/org/example/BankMain.java
@@ -12,46 +12,50 @@ public class BankMain {
     static Customer customer = new Customer(1, "정채은", account);
     public static void main(String[] args) {
         BankMain bankMain = new BankMain();
+        System.out.println("어서오세요 솝트뱅크입니다.");
         bankMain.startBank();
     }
 
     // 은행 시스템 시작
     public void startBank() {
-        System.out.println("어서오세요 솝트뱅크입니다.");
-        System.out.println("┌───원하시는 서비스를 선택해 주세요.───┐");
-        System.out.println("|           1. 계좌 이체         |");
-        System.out.println("|           2. 입금             |");
-        System.out.println("|           3. 출금             |");
-        System.out.println("|           0. 종료             |");
-        System.out.println("└──────────────────────────────┘");
+        boolean isRunning = true;
+        while (isRunning) {
+            System.out.println("┌───원하시는 서비스를 선택해 주세요.───┐");
+            System.out.println("|           1. 계좌 이체         |");
+            System.out.println("|           2. 입금             |");
+            System.out.println("|           3. 출금             |");
+            System.out.println("|           0. 종료             |");
+            System.out.println("└──────────────────────────────┘");
 
-        int bankService = scanner.nextInt();
+            int bankService = scanner.nextInt();
 
-        switch(bankService) {
-            case 1:
-                transfer();
-                break;
-            case 2:
-                deposit();
-                break;
-            case 3:
-                System.out.println("출금");
-                break;
-            case 0:
-                System.out.println("서비스를 종료합니다.");
-                end();
-                break;
-            default:
-                System.out.println("없는 선택입니다. 다시 선택해주세요.");
-                this.startBank();
-                break;
+            switch (bankService) {
+                case 1:
+                    transfer();
+                    break;
+                case 2:
+                    deposit();
+                    break;
+                case 3:
+                    withdraw();
+                    break;
+                case 0:
+                    System.out.println("서비스를 종료합니다.");
+                    end();
+                    isRunning = false;
+                    break;
+                default:
+                    System.out.println("없는 선택입니다. 다시 선택해주세요.");
+                    break;
+            }
         }
         scanner.close();
     }
 
+    // 계좌 이체
     public void transfer() {
-        boolean isTransferComplete = false;
-        while (!isTransferComplete) {
+        boolean isComplete = false;
+        while (!isComplete) {
             System.out.println("송금할 계좌 번호를 입력해 주세요");
             int accountNumber = scanner.nextInt();
             scanner.nextLine();
@@ -74,38 +78,8 @@ public class BankMain {
                     "\n잔액: " + (property - money));
             System.out.println("└──────────────────────────────┘");
 
-
             System.out.println("송금 정보가 맞나요?");
-            System.out.println("1. 네");
-            System.out.println("2. 아니요");
-            int confirm = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (confirm) {
-                case 1:
-                    System.out.println("비밀번호를 입력해 주세요");
-
-                    boolean passwordCorrect = false;
-                    while (!passwordCorrect) {
-                        int password = scanner.nextInt();
-                        scanner.nextLine();
-                        if (checkPassword(password)) {
-                            account.withdraw(money);
-                            System.out.println("송금이 완료되었습니다. " + "\n잔액: " + account.getProperty());
-                            passwordCorrect = true;
-                            isTransferComplete = true;
-                            break;
-                        } else {
-                            System.out.println("비밀번호가 틀렸습니다. 다시 입력해 주세요.");
-                        }
-                    }
-                    break;
-                case 2:
-                    System.out.println("정보를 다시 입력해 주세요.");
-                    break;
-                default:
-                    System.out.println("없는 선택지입니다. 다시 시도해 주세요.");
-            }
+            isComplete = checkInfo("송금", money, isComplete);
         }
     }
 
@@ -118,10 +92,73 @@ public class BankMain {
         System.out.println("입금이 완료되었습니다. " + "\n잔액: " + account.getProperty());
     }
 
+    // 출금
+    public void withdraw() {
+        boolean isComplete = false;
+        while (!isComplete) {
+            System.out.println("출금할 금액을 입력해 주세요.");
+            int money = scanner.nextInt();
+            scanner.nextLine();
+
+            int property = account.getProperty();
+            if (property - money < 0) {
+                System.out.println("잔액이 부족합니다. 다시 시작해 주세요.");
+                startBank();
+                break;
+            }
+
+            System.out.println("출금할 금액을 확인해 주세요.");
+            System.out.println("┌──────────────────────────────┐");
+            System.out.println("출금할 금액: " + money +
+                    "\n잔액: " + (property - money));
+            System.out.println("└──────────────────────────────┘");
+
+            System.out.println("출금 정보가 맞나요?");
+            isComplete = checkInfo("출금", money, isComplete);
+        }
+    }
+
     // 비밀번호 확인
     public boolean checkPassword(Integer password) {
         return account.getPassword().equals(password);
     }
+
+    // 정보 확인
+    public boolean checkInfo(String info, Integer money, boolean isComplete) {
+        System.out.println("1. 네");
+        System.out.println("2. 아니요");
+
+        int confirm = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (confirm) {
+            case 1:
+                System.out.println("비밀번호를 입력해 주세요");
+
+                boolean passwordCorrect = false;
+                while (!passwordCorrect) {
+                    int password = scanner.nextInt();
+                    scanner.nextLine();
+                    if (checkPassword(password)) {
+                        account.withdraw(money);
+                        System.out.println(info + "이 완료되었습니다. " + "\n잔액: " + account.getProperty());
+                        passwordCorrect = true;
+                        isComplete = true;
+                        break;
+                    } else {
+                        System.out.println("비밀번호가 틀렸습니다. 다시 입력해 주세요.");
+                    }
+                }
+                break;
+            case 2:
+                System.out.println("정보를 다시 입력해 주세요.");
+                break;
+            default:
+                System.out.println("없는 선택지입니다. 다시 시도해 주세요.");
+        }
+        return isComplete;
+    }
+
 
     // 은행 종료
     public void end() {
