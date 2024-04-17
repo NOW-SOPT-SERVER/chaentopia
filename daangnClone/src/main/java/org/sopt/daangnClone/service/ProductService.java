9a73c4c6afa14fe05e3c684f200cb012b27f3c1f
정채은 <cce523@hanmail.net>
2024-04-17 22:07:02 +0900

    Feat [#5] service 구현

--- daangnClone/src/main/java/org/sopt/daangnClone/service/ProductService.java
+++ daangnClone/src/main/java/org/sopt/daangnClone/service/ProductService.java
@@ -1,2 +1,24 @@
-package org.sopt.daangnClone.service.dto;public class ProductService {
+package org.sopt.daangnClone.service;
+
+import jakarta.transaction.Transactional;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.sopt.daangnClone.domain.Product;
+import org.sopt.daangnClone.repository.ProductRepository;
+import org.sopt.daangnClone.service.dto.ProductSellDto;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Transactional
+    public String createProduct(
+            ProductSellDto productSellDto
+    ) {
+        Product product = Product.create(productSellDto.title(),productSellDto.isSell(), productSellDto.price(), productSellDto.suggest(), productSellDto.description(), productSellDto.location() );
+        productRepository.save(product);
+        return product.getId().toString();
+    }
 }
