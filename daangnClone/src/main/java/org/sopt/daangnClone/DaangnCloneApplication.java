9cb96b4a2e4dbb7827e016325c2ce878575df490
정채은 <cce523@hanmail.net>
2024-04-17 14:36:21 +0900

    Setting [#5] 프로젝트 생성

---
+++ daangnClone/src/main/java/org/sopt/daangnClone/DaangnCloneApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.daangnClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnCloneApplication.class, args);
+	}
+
+}
---
+++ daangnClone/src/test/java/org/sopt/daangnClone/DaangnCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.daangnClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
