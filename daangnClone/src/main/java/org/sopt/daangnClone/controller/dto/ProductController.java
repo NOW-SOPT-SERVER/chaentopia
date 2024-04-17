af46705e2b38a1d6d01a37e939ac3e260ca3d15a
정채은 <cce523@hanmail.net>
2024-04-17 22:07:10 +0900

    Feat [#5] controller 구현

--- daangnClone/src/main/java/org/sopt/daangnClone/controller/dto/ProductController.java
+++ daangnClone/src/main/java/org/sopt/daangnClone/controller/dto/ProductController.java
@@ -1,4 +1,27 @@
 package org.sopt.daangnClone.controller.dto;
 
+import lombok.RequiredArgsConstructor;
+import org.sopt.daangnClone.service.ProductService;
+import org.sopt.daangnClone.service.dto.ProductSellDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/product")
 public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity createProduct(
+            @RequestBody ProductSellDto productSellDto
+    ) {
+        return ResponseEntity.created(URI.create(productService.createProduct(productSellDto)))
+                .build();
+    }
 }
